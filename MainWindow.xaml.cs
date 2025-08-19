daffa2789d756a948c6176831e25ae66f9b94686
altan <60016304+alcho75@users.noreply.github.com>
2025-08-19 16:08:40 +0300

    "remove duplicated log files" feature added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,193 +6,217 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace CLTL
+namespace CLTL;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window, INotifyPropertyChanged
+	private List<int> searchList = new();
+	public ObservableCollection<TLEvent> TLEventsStorage { get; set; } = [];
+	public ObservableCollection<TLEvent> TLEvents { get; set; } = [];
+	public string SearchResults { get => $"{SearchIndex} / {SearchCount}"; }
+	public int SearchIndex { get; set; }
+	public int SearchCount { get; set; }
+
+	const int TLEventControlCount = 50;
+
+	public MainWindow()
 	{
-		private List<int> searchList = new();
-		public ObservableCollection<TLEvent> TLEventsStorage { get; set; } = [];
-		public ObservableCollection<TLEvent> TLEvents { get; set; } = [];
-		public string SearchResults { get => $"{SearchIndex} / {SearchCount}"; }
-		public int SearchIndex { get; set; }
-		public int SearchCount { get; set; }
+		for (int i = 0; i < TLEventControlCount; i++) TLEvents.Add(new());
 
-		const int TLEventControlCount = 50;
+		InitializeComponent();
+	}
 
-		public MainWindow()
-		{
-			for (int i = 0; i < TLEventControlCount; i++) TLEvents.Add(new());
+	private void Window_Loaded(object sender, RoutedEventArgs e)
+	{
+		Reload();
+	}
 
-			InitializeComponent();
-		}
+	private void Reload()
+	{
+		LoadLogs();
+		slider.Focus();
 
-		private void Window_Loaded(object sender, RoutedEventArgs e)
-		{
-			Reload();
-		}
-
-		private void Reload()
-		{
-			LoadLogs();
-			slider.Focus();
-
-			if(searchBox.Text != "")
-			{
-				Search();
-			}
-		}
-
-		TLEvent? prvTLE = null;
-
-		public event PropertyChangedEventHandler? PropertyChanged;
-
-		void LoadFile(string filePath)
-		{
-			if (!File.Exists(filePath)) return;
-
-			foreach (string line in File.ReadAllLines(filePath))
-			{
-				TLEvent? tle = TLEvent.FromString(line);
-				if (tle != null)
-				{
-					if (prvTLE != null) prvTLE.Interval = tle.ms - prvTLE.ms;
-					if (!FindDuplicate(tle))
-					{
-						TLEventsStorage.Add(tle);
-						prvTLE = tle;
-					}
-				}
-			}
-		}
-
-
-		private bool FindDuplicate(TLEvent tle)
-		{
-			foreach (TLEvent tl in TLEventsStorage.TakeLast(1000))
-				if(tl.ms == tle.ms)
-					if(tl.Description == tle.Description)
-						return true;
-
-			return false;
-		}
-
-		void LoadLogs()
-		{
-			OpenFolderDialog ofd = new OpenFolderDialog();
-			bool? ok = ofd.ShowDialog();
-			if (ok == null) return;
-			if (ok == true)
-			{
-				TLEventsStorage.Clear();
-
-				List<string> files = [.. Directory.GetFiles(ofd.FolderName, "*.log")];
-				files.Sort();
-
-				foreach (string path in files) LoadFile(path);
-
-				Title += $" - {TLEventsStorage.Count} lines";
-
-				slider.Value = 0;
-				if (TLEventsStorage.Count > TLEventControlCount)
-				{
-					slider.IsEnabled = true;
-					slider.Maximum = TLEventsStorage.Count - TLEventControlCount;
-					slider.SmallChange = 1;
-					slider.LargeChange = slider.SmallChange * TLEventControlCount;
-				}
-				else slider.IsEnabled = false;
-
-				SetEventsView(0);
-			}
-		}
-
-		private void SetEventsView(int startIndex)
-		{
-			for (int i = 0; i < TLEventControlCount; i++)
-			{
-				int index = i + startIndex;
-				if (index < TLEventsStorage.Count)
-				{
-					TLEvents[i] = TLEventsStorage[index];
-				}
-				else TLEvents[i].Clear();
-			}
-		}
-
-		private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			SetEventsView((int)e.NewValue);
-		}
-
-		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		if(searchBox.Text != "")
 		{
 			Search();
 		}
+	}
 
-		private void Search()
-		{
-			SearchCount = Serach(searchBox.Text);
-			PropertyChanged?.Invoke(this, new(nameof(SearchResults)));
-		}
+	TLEvent? prvTLE = null;
 
-		private int Serach(string text)
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	void LoadFile(string filePath)
+	{
+		if (!File.Exists(filePath)) return;
+
+		foreach (string line in File.ReadAllLines(filePath))
 		{
-			SearchIndex = 0;
-			int sc = 0;
-			int i = 0;
-			searchList.Clear();
-			foreach(TLEvent tle in TLEventsStorage)
+			TLEvent? tle = TLEvent.FromString(line);
+			if (tle != null)
 			{
-				tle.SearchMatch = false;
-				if (text != "")
-				{
-					if (tle.Description.Contains(text) || (tle.Interval.ToString().Contains(text)))
-					{
-						tle.SearchMatch = true;
-						searchList.Add(i);
-						sc++;
-					}
-				}
-				i++;
+				if (prvTLE != null) prvTLE.Interval = tle.ms - prvTLE.ms;
+				TLEventsStorage.Add(tle);
+				prvTLE = tle;
 			}
-			return sc;
-		}
-
-		private void searchBox_PreviewKeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Enter)
-			{
-				if (SearchCount == 0) return;
-
-				if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
-				{
-					if (SearchIndex < SearchCount) SearchIndex++;
-					else SearchIndex = 1;
-				}
-				else
-				{
-					if (SearchIndex > 1) SearchIndex--;
-					else SearchIndex = SearchCount;
-				}
-				PropertyChanged?.Invoke(this, new(nameof(SearchResults)));
-				if (SearchIndex > 0)
-				{
-					UpdateSlider(searchList[SearchIndex - 1]);
-				}
-			}
-		}
-
-		private void UpdateSlider(int startIndex)
-		{
-			slider.Value = startIndex;
-		}
-
-		private void MenuItem_Click(object sender, RoutedEventArgs e)
-		{
-			Reload();
 		}
 	}
+
+	void LoadLogs()
+	{
+		OpenFolderDialog ofd = new OpenFolderDialog();
+		bool? ok = ofd.ShowDialog();
+		if (ok == null) return;
+		if (ok == true)
+		{
+			TLEventsStorage.Clear();
+
+			List<string> files = [.. Directory.GetFiles(ofd.FolderName, "*.log")];
+			files.Sort();
+
+			// check duplicate files
+			RemoveDuplicateFiles(files);
+
+			// load log files
+			foreach (string path in files) LoadFile(path);
+
+			Title += $" - {TLEventsStorage.Count} lines";
+
+			slider.Value = 0;
+			if (TLEventsStorage.Count > TLEventControlCount)
+			{
+				slider.IsEnabled = true;
+				slider.Maximum = TLEventsStorage.Count - TLEventControlCount;
+				slider.SmallChange = 1;
+				slider.LargeChange = slider.SmallChange * TLEventControlCount;
+			}
+			else slider.IsEnabled = false;
+
+			SetEventsView(0);
+		}
+	}
+
+	private void RemoveDuplicateFiles(List<string> files)
+	{
+		int fi = 0;
+
+		while (fi < files.Count-1) // at least two files
+		{
+			string[] left = File.ReadAllLines(files[fi]);
+			string[] right = File.ReadAllLines(files[fi + 1]);
+
+			int duplicates = 0;
+			int li = 0;
+			int lim = Math.Min(left.Length, right.Length);
+			while (li < lim)
+			{
+				if (left[li] == right[li]) ++duplicates;
+				++li;
+			}
+
+			if(duplicates == lim) // all lines are same
+			{
+				int ri = (lim == left.Length) ? fi : fi + 1;
+				string fp = files[ri];
+				files.RemoveAt(ri);
+
+				MessageBox.Show($"Duplicated file removed: {fp}");
+
+				continue; // cont. with same index
+			}
+
+			++fi;
+		}
+	}
+
+	private void SetEventsView(int startIndex)
+	{
+		for (int i = 0; i < TLEventControlCount; i++)
+		{
+			int index = i + startIndex;
+			if (index < TLEventsStorage.Count)
+			{
+				TLEvents[i] = TLEventsStorage[index];
+			}
+			else TLEvents[i].Clear();
+		}
+	}
+
+	private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	{
+		SetEventsView((int)e.NewValue);
+	}
+
+	private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		Search();
+	}
+
+	private void Search()
+	{
+		SearchCount = Search(searchBox.Text);
+		PropertyChanged?.Invoke(this, new(nameof(SearchResults)));
+	}
+
+	private int Search(string text)
+	{
+		SearchIndex = 0;
+		int sc = 0;
+		int i = 0;
+		searchList.Clear();
+		foreach(TLEvent tle in TLEventsStorage)
+		{
+			tle.SearchMatch = false;
+			if (text != "")
+			{
+				if (tle.Description.Contains(text) || (tle.Interval.ToString().Contains(text)))
+				{
+					tle.SearchMatch = true;
+					searchList.Add(i);
+					sc++;
+				}
+			}
+			i++;
+		}
+		return sc;
+	}
+
+	private void searchBox_PreviewKeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			if (SearchCount == 0) return;
+
+			if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+			{
+				if (SearchIndex < SearchCount) SearchIndex++;
+				else SearchIndex = 1;
+			}
+			else
+			{
+				if (SearchIndex > 1) SearchIndex--;
+				else SearchIndex = SearchCount;
+			}
+			PropertyChanged?.Invoke(this, new(nameof(SearchResults)));
+			if (SearchIndex > 0)
+			{
+				UpdateSlider(searchList[SearchIndex - 1]);
+			}
+		}
+	}
+
+	private void UpdateSlider(int startIndex)
+	{
+		slider.Value = startIndex;
+	}
+
+	private void MenuItem_Click(object sender, RoutedEventArgs e)
+	{
+		Reload();
+	}
+
 }
+
